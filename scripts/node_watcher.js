0cf8df737ad5b1816c06f9493fcc3a37c6a0be2a
Melvin Sowah <mxtcaps@gmail.com>
2016-08-29 09:31:51 +0100

    Preparing to switch to Kefir from Highland

---
+++ scripts/node_watcher.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const global = self;
+
+(function (CommentsPlus) {
+
+  const NodeWatcher = CommentsPlus.NodeWatcher = {
+    new: function (inputSelector, outputSelector) {
+      const self = Object.create(this);
+
+
+
+      return self;
+    }
+  };
+
+})(global.CommentsPlus = global.CommentsPlus || {});
